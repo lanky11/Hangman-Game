03921547b865239daded85eeb0e057506a1acd05
Leif Swenson <leifswenson@gmail.com>
2017-11-28 22:50:02 +0000

    more work on js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,37 +3,63 @@
 // Creates an array that lists all the possible words. 
 var wordChoices = ["tiger", "nicklaus", "palmer", "mickelson", "spieth", "mcilroy"];
 
+
 // Create a variable to hold the number of wins.
 var wins = 0;
 
-// Create an empty array to hold guessed letters.
-var guessedLetters = [];
+
+// Create an empty array to hold incorrect guessed letters.
+var incorrectGuessedLetters = [];
+
 
 // Create a counter and subtracte one each time an incorret guess it given
- var guessesRemaining = 8;
+var guessesRemaining = 10;
 
 
-// Pick a word from the wordChoices array
-var pickedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-console.log(pickedWord);
-
-// Break up the word into an array
-var pickedWordArray = pickedWord.split("");
-console.log(pickedWordArray);
-
-
-// if else statement to see if a guessed word is within the pickedWordArray if its not push it into the 
-
-
-
-// use a loop to create a dash for each letter in the array
-
-
-// listen for the letter that user types
-
+// var that represents the number of dashes for every letter in picked word
 
 
 
 document.onkeyup = function(event) {
   
-}
+  // listen for the letter that user types
+  var userGuess = event.key.toLowerCase();
+  console.log(userGuess);
+
+
+  // Pick a word from the wordChoices array
+  var pickedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+  console.log(pickedWord);
+
+  // Break up the word into an array
+  var pickedWordArray = pickedWord.split("");
+  console.log(pickedWordArray);
+  
+  
+  // maybe use a loop to create a dash for each letter in the array
+  
+  // if else statement to see if a guessed letter is within the pickedWordArray if its not push it into the incorrectGuessedLetters
+  if (pickedWord.indexOf(userGuess) === -1) {
+    incorrectGuessedLetters.push(userGuess);
+    guessesRemaining--;
+    // if letter has already been guessed do not push it into the array again
+  } else {
+    // not sure now to replace dash with letter from picked word
+  }
+
+
+  // if guessesRemaining === 0 end game
+
+
+  // html to inject into html file
+  var html =
+    "<li>Wins: " + wins + "</li>" +
+    "<li>Current word: " + "_ _ _"  + "</li>" +
+    "<li>Number of Guesses Remaining: " + guessesRemaining + "</li>" +
+    "<li>Letters Already Guessed: " + incorrectGuessedLetters + "</li>";
+
+  // Set the inner HTML contents of the #game div to our html string
+  document.querySelector("#game").innerHTML = html;
+
+
+};
