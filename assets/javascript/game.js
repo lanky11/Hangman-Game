d73d2bab9dfe8051a1d7a529adc7a1f9d7739959
Leif Swenson <leifswenson@gmail.com>
2017-11-28 20:29:32 +0000

    started work on javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,39 @@
 
 
+// Creates an array that lists all the possible words. 
+var wordChoices = ["tiger", "nicklaus", "palmer", "mickelson", "spieth", "mcilroy"];
 
-console.log("Hangman Golf Game");
+// Create a variable to hold the number of wins.
+var wins = 0;
+
+// Create an empty array to hold guessed letters.
+var guessedLetters = [];
+
+// Create a counter and subtracte one each time an incorret guess it given
+ var guessesRemaining = 8;
+
+
+// Pick a word from the wordChoices array
+var pickedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+console.log(pickedWord);
+
+// Break up the word into an array
+var pickedWordArray = pickedWord.split("");
+console.log(pickedWordArray);
+
+
+// if else statement to see if a guessed word is within the pickedWordArray if its not push it into the 
 
 
 
+// use a loop to create a dash for each letter in the array
+
+
+// listen for the letter that user types
+
+
+
+
+document.onkeyup = function(event) {
+  
+}
