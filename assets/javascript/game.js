d9266e432d8fd04f745ab7c45313921724a3dfc9
Leif Swenson <leifswenson@gmail.com>
2017-12-01 23:25:54 +0000

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 
-
 // Creates an array that lists all the possible words. 
 var wordChoices = ["tiger", "nicklaus", "palmer", "mickelson", "spieth", "mcilroy"];
 
+// Possible guessed letter
+
+var validKeys = "abcdefghijklmnopqrstuvwxy".split('');
 
 // Create a variable to hold the number of wins.
 var wins = 0;
@@ -37,7 +39,14 @@ function makedash () {
   }
 }
 
-// reset game
+// function to reset the game
+function gameReset() {
+  var pickedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+  guessesRemaining = 10
+  incorrectGuessedLetters = []
+  
+}
+
 
 
 document.onkeyup = function(event) {
@@ -46,6 +55,16 @@ document.onkeyup = function(event) {
   var userGuess = event.key.toLowerCase();
   console.log(userGuess);
 
+  // check if the guess was valid
+  
+  if (validKeys.indexOf(userGuess) === -1) {
+    alert("Invalid guess.  Please select a letter from a-z")
+  } else if ((pickedWordArray.indexOf(userGuess) === -1) && (incorrectGuessedLetters.indexOf(userGuess) === -1)) {
+    
+  } else {
+    
+  }
+  
   
   
   // if else statement to see if a guessed letter is within the pickedWordArray if its not push it into the incorrectGuessedLetters
@@ -68,7 +87,7 @@ document.onkeyup = function(event) {
     
   // html to inject into html file for current word
   var htmlCurrentWord =
-    "Current word: " + currentWord;
+    "Current word: " + pickedWordArray;
     
   // html to inject into html file for guesses remaining
   var htmlGuessesRemaining =
@@ -78,11 +97,11 @@ document.onkeyup = function(event) {
   var htmlIncorrectGuessedLetters =
     "Letters Already Guessed: " + incorrectGuessedLetters;
 
-  // Set the inner HTML contents of the #game div to our html string
-  document.querySelector("#wins").innerHTML = htmlWins;
-  document.querySelector("#current-word").innerHTML = htmlCurrentWord;
-  document.querySelector("#guesses-remaining").innerHTML = htmlGuessesRemaining;
-  document.querySelector("#incorrect-letters").innerHTML = htmlIncorrectGuessedLetters;
+  // Set the inner HTML contents of the differen elements
+  document.getElementById("wins").innerHTML = htmlWins;
+  document.getElementById("current-word").innerHTML = htmlCurrentWord;
+  document.getElementById("guesses-remaining").innerHTML = htmlGuessesRemaining;
+  document.getElementById("incorrect-letters").innerHTML = htmlIncorrectGuessedLetters;
 
 
 };
