ff91177e13961b30b2ab3685936b34eb0a6fef17
Leif Swenson <leifswenson@gmail.com>
2017-12-04 22:25:27 +0000

    clean up of js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,27 @@
+// Variables
+//=======================================================================================
 
 // Creates an array that lists all the possible words. 
 var wordChoices = ["tiger", "nicklaus", "palmer", "mickelson", "spieth", "mcilroy"];
+var currentWord = "";
+var lettersInWord = [];
+var numberLetters = 0;
 
 // Possible guessed letter
-
 var validKeys = "abcdefghijklmnopqrstuvwxy".split('');
 
-// Create a variable to hold the number of wins.
+// Create a variables for wins
 var wins = 0;
-
+var losses = 0;
+var guessesRemaining = 9;
 
 // Create an empty array to hold incorrect guessed letters.
-var incorrectGuessedLetters = [];
-
+var incorrectGuesses = [];
 
 // Create a counter and subtracte one each time an incorret guess it given
-var guessesRemaining = 10;
+
+
+// number of letters in the word
 
 
 // Pick a word from the wordChoices array
@@ -28,26 +34,24 @@ var pickedWordArray = pickedWord.split("");
 console.log(pickedWordArray);
 
 
-// var that represents the number of dashes for every letter in picked word
-var currentWord = [];
 
+//Functions
+//=======================================================================================
 
 // a loop to create a dash to represent each letter in the array
-function makedash () {
-  for (var i=0; i < pickedWordArray.length; i++) {
-    currentWord.push("_")
-  }
-}
+
 
 // function to reset the game
 function gameReset() {
   var pickedWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-  guessesRemaining = 10
-  incorrectGuessedLetters = []
+  guessesRemaining = 9
+  incorrectGuesses = []
   
 }
 
 
+//KeyUp Action
+//=======================================================================================
 
 document.onkeyup = function(event) {
   
@@ -59,7 +63,7 @@ document.onkeyup = function(event) {
   
   if (validKeys.indexOf(userGuess) === -1) {
     alert("Invalid guess.  Please select a letter from a-z")
-  } else if ((pickedWordArray.indexOf(userGuess) === -1) && (incorrectGuessedLetters.indexOf(userGuess) === -1)) {
+  } else if ((pickedWordArray.indexOf(userGuess) === -1) && (incorrectGuesses.indexOf(userGuess) === -1)) {
     
   } else {
     
@@ -67,9 +71,9 @@ document.onkeyup = function(event) {
   
   
   
-  // if else statement to see if a guessed letter is within the pickedWordArray if its not push it into the incorrectGuessedLetters
+  // if else statement to see if a guessed letter is within the pickedWordArray if its not push it into the incorrectGuesses
   if (pickedWordArray.indexOf(userGuess) === -1) {
-    incorrectGuessedLetters.push(userGuess);
+    incorrectGuesses.push(userGuess);
     guessesRemaining--;
     // if letter has already been guessed do not push it into the array again
     
@@ -81,27 +85,10 @@ document.onkeyup = function(event) {
   // if guessesRemaining === 0 end game
 
 
-  // html to inject into html file for wins
-  var htmlWins =
-    "Wins: " + wins;
-    
-  // html to inject into html file for current word
-  var htmlCurrentWord =
-    "Current word: " + pickedWordArray;
-    
-  // html to inject into html file for guesses remaining
-  var htmlGuessesRemaining =
-    "Number of Guesses Remaining: " + guessesRemaining;
-    
-  // html to inject into html file for incorrect guessed letters
-  var htmlIncorrectGuessedLetters =
-    "Letters Already Guessed: " + incorrectGuessedLetters;
-
-  // Set the inner HTML contents of the differen elements
-  document.getElementById("wins").innerHTML = htmlWins;
-  document.getElementById("current-word").innerHTML = htmlCurrentWord;
-  document.getElementById("guesses-remaining").innerHTML = htmlGuessesRemaining;
-  document.getElementById("incorrect-letters").innerHTML = htmlIncorrectGuessedLetters;
-
-
+  // Set the inner HTML contents of the different elements
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("losses").innerHTML = losses;
+  document.getElementById("current-word").innerHTML = pickedWordArray;
+  document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+  document.getElementById("incorrect-letters").innerHTML = incorrectGuesses;
 };
